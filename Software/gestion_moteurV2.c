5a11afffc2723de3df6a50bb5980b3a954e01155
ThomasAbg <01thomas.abgrall@gmail.com>
2018-12-05 16:35:38 +0100

    start Finite-state machine

--- Software/gestion_moteurV2.c
+++ Software/gestion_moteurV2.c
@@ -1,6 +1,11 @@
-/************************************************************************\
+//-----------------------------------------------------------------------------------------------------------------------//
+// Projet: Autoscope
+// Autor: Thomas ABGRALL
+// Subject: Driver Linux of steps motors
+// Date: 2018-2019
+// IDE: Pluma
+//-----------------------------------------------------------------------------------------------------------------------//
 
-\************************************************************************/
 #include <linux/kernel.h> /* Needed for KERN_INFO */
 #include <linux/init.h> /* Needed for the macros */
 #include <linux/module.h>
@@ -8,39 +13,48 @@
 #include <linux/gpio.h>
 #include <linux/fs.h>
 #include "header.h"
-
-// Variable par pin
+//-----------------------------------------------------------------------------------------------------------------------//
+//--------------------------------------------------Variable par pin--------------------------------------------------//
 // 	Contrôle moteur rotation
-#define Step_MRotation 8
-#define Dir_MRotation 10
-#define Sleep_MRotation 12
+#define Step_MRotation 16
+#define Dir_MRotation 18
+#define Sleep_MRotation 22
 // 	Contrôle moteur inclinaison
-#define Step_MTilt 11
-#define Dir_MTilt 13
-#define Sleep_MTilt 15
+#define Step_MTilt 24
+#define Dir_MTilt 26
+#define Sleep_MTilt 28
+//-----------------------------------------------------------------------------------------------------------------------//
 
+//-----------------------------------------------------------------------------------------------------------------------//
+//--------------------------------------------------Prototype fonctions--------------------------------------------------//
 static void TimerHandler_Rotation(unsigned long unused);	// controle du moteur de rotation
 static void TimerHandler_Tilt(unsigned long unused); // controle des moteurs d'inclinaison
+//-----------------------------------------------------------------------------------------------------------------------//
 
+//-----------------------------------------------------------------------------------------------------------------------//
+//----------------------------------------------Création variables globales----------------------------------------------//
 t_engine MoteurRotation, MoteurTilt;	// définition des moteurs en variables globales
 static struct timer_list timerMotRot, timerMotTilt;		// timer des moteurs
 int TableauPin[6] = {Step_MRotation, Dir_MRotation, Sleep_MRotation, Step_MTilt, Dir_MTilt, Sleep_MTilt}; 
 union Etat {		// machine état 
 char CharEtat;			// sous forme de char
-struct {unsigned int Rotation :1;		// état du moteur de rotation 1 = en rotation 0 = à l'arret
-		unsigned int Tilt :1;			// état du moteur de d'inclinaison 1 = en rotation 0 = à l'arret
-		unsigned int ErreurTimer :1;	// état si il y a une erreur timer = 1 sinon = 0
-		unsigned int ErreurGPIO :1;		// état si il y a une erreur d'écriture/lecture avec les I/O = 1 sinon = 0
-		unsigned int unused :28;  // pour un int de 32 bit, emplacement libre
+struct {unsigned char Rotation :1;		// état du moteur de rotation 1 = en rotation 0 = à l'arret
+		unsigned char Tilt :1;			// état du moteur de d'inclinaison 1 = en rotation 0 = à l'arret
+		unsigned char ErreurTimer :1;	// état si il y a une erreur timer = 1 sinon = 0
+		unsigned char ErreurGPIO :1;	// état si il y a une erreur d'écriture/lecture avec les I/O = 1 sinon = 0
+		unsigned char unused :4;  		// complete le reste du char avec 4 bits libre
 		}Moteur;
 };
 	//t_engine *TableOfMotor[2] = {&MoteurRotation, &MoteurTilt};
 //int time_interval = 1;
 
-static int __init my_init (void)	// initilaisation des timers  //**** j'ai enlever static ****
+//-----------------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------Fonction initialisation du driver-------------------------------------------//
+static int __init my_init (void)	// initilaisation des timers 
 {
+	printk(KERN_INFO "Initailisation du driver moteur");
 	int err, i;
-	for(i=1; i < sizeof(TableauPin); i++){	// parcours toutees les PINs
+	for(i=1; i < sizeof(TableauPin); i++){	// parcours toutes les PINs
 		if ((err = gpio_request(TableauPin[i],THIS_MODULE->name)) != 0) {	
 			gpio_free(TableauPin[i]);  // libère la PIN
 			return err;
@@ -49,44 +63,67 @@ static int __init my_init (void)	// initilaisation des timers  //**** j'ai enlev
 			gpio_free(TableauPin[i]);
 			return err;
 		}
-		return 0;
 	} 
+	rotate(100000, clockwise);
 	return 0;
 }
+//-----------------------------------------------------------------------------------------------------------------------//
 
-unsigned int rotate(t_engine engine, unsigned int pas, e_direction direction) // initialiser moteur et lance timer
+//-----------------------------------------------------------------------------------------------------------------------//
+//------------------------------------------------Fonction lance rotation------------------------------------------------//
+unsigned int rotate(unsigned int pas, e_direction direction) // initialiser moteur et lance timer
 {
-	// configuration pin moteur rotation
-	MoteurRotation.pin_step = Step_MRotation;	// numéro pin step moteur rotation
-	MoteurRotation.pin_dir	= Dir_MRotation;	// numéro pin direction 
-	MoteurRotation.pin_sleep= Sleep_MRotation;	// numéro pin sleep
-	MoteurRotation.numberStep=pas;	// initialisation du nombre de step a parcourir
-	MoteurRotation.currentNumberStep=0;	// initialisation du nombre de step accompli
+	if(Etat.Moteur.Rotation == 0){
+		printk(KERN_INFO "Driver: open()\n");
+		// configuration pin moteur rotation
+		MoteurRotation.pin_step = Step_MRotation;	// numéro pin step moteur rotation
+		MoteurRotation.pin_dir	= Dir_MRotation;	// numéro pin direction 
+		MoteurRotation.pin_sleep= Sleep_MRotation;	// numéro pin sleep
+		MoteurRotation.numberStep=pas;		// initialisation du nombre de step a parcourir
+		MoteurRotation.currentNumberStep=0;	// initialisation du nombre de step accompli
 
-	init_timer(& timerMotRot);	// création du timer pour le moteur rotation
-	timerMotRot.function = TimerHandler_Rotation;	// assiniation de la fonction au timer
-	add_timer(& timerMotRot);	// ajout du timer (mise en route du timer)
-	return 1;
+		init_timer(& timerMotRot);	// création du timer pour le moteur rotation
+		timerMotRot.function = TimerHandler_Rotation;	// assiniation de la fonction au timer
+		add_timer(& timerMotRot);	// ajout du timer (mise en route du timer)
+	}
+	else{
+		printk(KERN_INFO "Entré dans inclinate");
+	}
+	return 0;
 }
+//-----------------------------------------------------------------------------------------------------------------------//
 
-unsigned int inclinate(t_engine engine1, unsigned int pas, e_direction direction)//init moteur et lance timer
+//-----------------------------------------------------------------------------------------------------------------------//
+//----------------------------------------------Fonction lance inclinaison-----------------------------------------------//
+unsigned int inclinate(unsigned int pas, e_direction direction)//init moteur et lance timer
 {
-	// configuration pin moteur inclinaison
-	MoteurTilt.pin_step = Step_MTilt;	// numéro pin step moteur rotation
-	MoteurTilt.pin_dir	= Dir_MTilt;	// numéro pin direction 
-	MoteurTilt.pin_sleep= Sleep_MTilt;	// numéro pin sleep
-	MoteurTilt.numberStep=pas;	// initialisation du nombre de step a parcourir
-	MoteurTilt.currentNumberStep=0;	// initialisation du nombre de step accompli
-	
-	init_timer(& timerMotTilt);	// création du timer pour le moteur rotation
-	//timer_setup(&timerMotTilt, TimerHandler_Tilt, 0);
-	timerMotTilt.function = TimerHandler_Tilt;	// assiniation de la fonction au timer
-	add_timer(& timerMotTilt);	// ajout du timer (mise en route du timer)
+	if(Etat.Moteur.Tilt == 0){
+		printk(KERN_INFO "Entré dans inclinate");
+		// configuration pin moteur inclinaison
+		MoteurTilt.pin_step = Step_MTilt;	// numéro pin step moteur rotation
+		MoteurTilt.pin_dir	= Dir_MTilt;	// numéro pin direction 
+		MoteurTilt.pin_sleep= Sleep_MTilt;	// numéro pin sleep
+		MoteurTilt.numberStep=pas;	// initialisation du nombre de step a parcourir
+		MoteurTilt.currentNumberStep=0;	// initialisation du nombre de step accompli
+		
+		init_timer(& timerMotTilt);	// création du timer pour le moteur rotation
+		//timer_setup(&timerMotTilt, TimerHandler_Tilt, 0);
+		timerMotTilt.function = TimerHandler_Tilt;	// assiniation de la fonction au timer
+		add_timer(& timerMotTilt);	// ajout du timer (mise en route du timer)
+	}
+	else{
+		printk(KERN_INFO "Entré dans inclinate");
+	}
 	return 0;
 }	
+//-----------------------------------------------------------------------------------------------------------------------//
 
+//#######################################################################################################################//
+//---------------------------------------Fonctions Appeler a la fin de leur timer----------------------------------------//
 static void TimerHandler_Rotation(unsigned long unused)	// controle du moteur de rotation
 {
+	Etat.Moteur.Rotation = 1;
+	printk(KERN_INFO "Handler rotation");
 	if(MoteurRotation.currentNumberStep < MoteurRotation.numberStep){	// controle distance parcouru
 		if(gpio_get_value(Step_MRotation) == 0){		// controle de l'état de la clk (haut ou bas)
 			gpio_set_value(Step_MRotation, 1);	// passe a 1 l'état de la clk 
@@ -99,11 +136,14 @@ static void TimerHandler_Rotation(unsigned long unused)	// controle du moteur de
 	}
 	else{	// le nombre de step moteur a été atteind
 		del_timer(& timerMotRot);	// destruction du timer pour ce moteur
-		}
+		Etat.Moteur.Rotation = 0;
+	}
 }
 
 static void TimerHandler_Tilt(unsigned long unused) // controle des moteurs d'inclinaison
 {
+	Etat.Moteur.Tilt = 1;
+	printk(KERN_INFO "Handler tilt");
 	if(MoteurTilt.currentNumberStep < MoteurTilt.numberStep){	
 		if(gpio_get_value(Step_MTilt) == 0){
 			gpio_set_value(Step_MTilt, 1);
@@ -116,11 +156,16 @@ static void TimerHandler_Tilt(unsigned long unused) // controle des moteurs d'in
 	}
 	else{
 		del_timer(& timerMotTilt);
+		Etat.Moteur.Tilt = 0;
 	}
 }
+//#######################################################################################################################//
 
+//-----------------------------------------------------------------------------------------------------------------------//
+//---------------------------------------------------Fonctions Arret-----------------------------------------------------//
 void stopOne(t_engine Motor)	// permet d'arrter un moteur au choix
 {
+	printk(KERN_INFO "Stop_One");
 	if(Motor.pin_step == MoteurRotation.pin_step){	// vérifie si le moteur rotation a été choisi
 		MoteurRotation.pin_sleep = 1;	// endort le moteur
 	}
@@ -131,19 +176,45 @@ void stopOne(t_engine Motor)	// permet d'arrter un moteur au choix
 
 void stopall()	// Cette fonction permet d'arreter immediatement tous les moteurs
 {
+	printk(KERN_INFO "stop all");
 	MoteurRotation.pin_sleep = 1;	// endort le moteur rotation
 	MoteurTilt.pin_sleep = 1;		// endort le moteur inclinaison
 }
+//-----------------------------------------------------------------------------------------------------------------------//
 
+//-----------------------------------------------------------------------------------------------------------------------//
+//---------------------------------------------------Fonctions Exit-----------------------------------------------------//
 static void __exit my_exit (void)	// destructeur de timer
 {
+	printk(KERN_INFO "EXIT");
 	del_timer(& timerMotRot);	// destruction du timer moteur rotation
 	gpio_free(Step_MRotation);	// libération de la PIN du step MRotation
 }
 
+switch (Etat.CharEtat):
+	case 0b10000000:	// moteur rotation en cour de rotation
+	
+		break;
+	case 0b01000000:	// moteur tilt en cour de rotation
+	
+		break;
+	case 0b00100000:	// erreur TIMER
+	
+		break;
+	case 0b00010000:	// erreur GPIO
+	
+		break;
+	default:			// Defaut
+		
+		break;
+
+//-----------------------------------------------------------------------------------------------------------------------//
+//--------------------------------------Fonctions lancer au démarrage du driver------------------------------------------//
 module_init(my_init);	// initialisation
+rotate(100000, clockwise);	// force la fonction rotation a l'initialisation du driver
 module_exit(my_exit);
 MODULE_LICENSE("GPL");
+//-----------------------------------------------------------------------------------------------------------------------//
 
 // ajouter la fonction de qualibration avec interrupteur
 // récupperer les valeurs de retour de chacune des fonctions principales
