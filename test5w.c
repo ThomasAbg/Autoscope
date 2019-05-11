39dffd238093c1f180071d84d1b55e2061aa5208
tlepoix@localhost <thomas.lepoix@gmail.com>
2019-05-11 23:20:39 +0200

    restore IPC test

--- test5w.c
+++ test5w.c
@@ -1,49 +1,48 @@
-//////////////////////////////////////////////////////////////////////GPS_DAEMON
+//////////////////////////////////////////////////////////////////AUTOSCOPE_CORE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <signal.h>
-#include <sys/stat.h>
-//#include <sys/types.h>
 
-char* gps_fifo="/tmp/gps_fifo";
-char flag_sending=0;
+#define GPS_PID_FILE	"./test.pid"		///var/run/mtk3339d.pid
+#define GPS_FIFO_FILE	"/tmp/gps_fifo"
+#define MAX_BUF			1024				//256
 
-void send_gps_datas(int sig) {
-	int fd;
-	char* message="Hi";
+void get_gps_pid(pid_t* gps_pid) {
+	FILE* f_gps_pid;
+	char buf[6]={}; //pid up to 5 digits + \0 | limits.h LINE_MAX
+	if(!(f_gps_pid=fopen(GPS_PID_FILE, "r"))) {
+		printf("ERROR: Can't open GPS daemon PID file\n");
+		exit(EXIT_FAILURE);
+		}
 
-	printf("SIGUSR1 received\n");
-	flag_sending=1;
-
-	fd = open(gps_fifo, O_WRONLY);
-	write(fd, message, sizeof(message));
-	printf("Send message : %s\n", message);
-	close(fd);
-
-	flag_sending=0;
+	fgets(buf, 6, f_gps_pid);
+	*gps_pid=atoi(buf);
+	fclose(f_gps_pid);
+//	printf("atoi : %d\n", *gps_pid);
 	}
 
-void end(int sig) {
-	unlink(gps_fifo);
-	printf("DONE\n");
-	exit(0);	
+void read_gps_data(pid_t* gps_pid){
+	int fd;
+	char* gps_fifo = GPS_FIFO_FILE;
+	char buf[MAX_BUF];
+
+	/*request to send data*/
+	kill(*gps_pid, SIGUSR1);
+
+	/*read through fifo*/
+	fd = open(gps_fifo, O_RDONLY);
+	read(fd, buf, MAX_BUF);
+	printf("Received message : %s\n", buf);
+	close(fd);
 	}
 
 void main(void) {
-	printf("PID : %d\n", getpid());
+	pid_t gps_pid=0;
+	get_gps_pid(&gps_pid);
+	printf("GPS daemon PID : %d\n", gps_pid);
 
-	if(signal(SIGUSR1, send_gps_datas) == SIG_ERR) {
-		printf("ERROR: Can't catch SIGUSR1\n");
-		exit(EXIT_FAILURE);
-		}
-	if(signal(SIGTERM, end) == SIG_ERR) {
-		printf("ERROR: Can't catch SIGTERM\n");
-		exit(EXIT_FAILURE);
-		}
-
-	mkfifo(gps_fifo, 0666);
-
-	while(1) {}
+	read_gps_data(&gps_pid);
 	}
+
