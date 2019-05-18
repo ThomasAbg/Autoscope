f15ba13b67da2a449d0172c0d76db81d4b37fbaa
Thomas <45203550+ThomasAbg@users.noreply.github.com>
2019-05-18 20:09:21 +0200

    Update header.h

--- header.h
+++ header.h
@@ -4,28 +4,26 @@
  *  Created on: 21 nov. 2018
  *      Author: potato
  */
- 
+
 #ifndef ENGINECONTROL_H_
 #define ENGINECONTROL_H_
 
 #include <linux/kernel.h> 
 #include <linux/interrupt.h>
 #include <linux/module.h>
+#include <linux/ioctl.h>
 #include <linux/timer.h>
 #include <linux/init.h> 
 #include <linux/gpio.h>
 #include <linux/fs.h>
-#include "header.h"
 
 #define PINNUMBER(port, pinNumber)	(((port) - 1) * 32 + (pinNumber))
 
 typedef struct{
 	unsigned int pin_step;		// pin number for step
 	unsigned int pin_dir;		// pin number for direction
-	unsigned int pin_sleep;		// pin number for sleep
-	unsigned int numberStep;	// number of step to do
-	unsigned int currentNumberStep; // current number of step did
-	
+	unsigned int number16Step;	// number of step to do
+	unsigned int currentnumber16Step; // current number of step did
 }t_engine;
 
 typedef enum {
@@ -37,21 +35,18 @@ unsigned int initEngine(void);
 
 void posInitial(void);
 
-//**************************************************************************
 
-unsigned int rotate(unsigned long pas, e_direction direction);
 
-unsigned int inclinate(unsigned long pas, e_direction direction);
 
-unsigned int zoom(unsigned long pas, e_direction direction); // initialiser moteur et lance timer
+typedef struct
+{
+    int status, dignity, ego;
+} query_arg_t;
+ 
+#define QUERY_GET_VARIABLES _IOR('q', 1, query_arg_t *)
+#define QUERY_CLR_VARIABLES _IO('q', 2)
+#define QUERY_SET_VARIABLES _IOW('q', 3, query_arg_t *)
 
-//**************************************************************************
-
-void TimerHandler_Rotation(unsigned long unused);	// controle du moteur de rotation
-
-void  TimerHandler_Tilt(unsigned long unused); // controle des moteurs d'inclinaison
-
-void TimerHandler_Zoom(unsigned long unused); // controle des moteurs d'inclinaison
 
 //**************************************************************************
 
@@ -67,7 +62,23 @@ irqreturn_t limit_switch_Zoom2(int irq, void * ident);
 
 //**************************************************************************
 
-int __init my_init(void);
+unsigned int rotate(unsigned long pas, e_direction direction);
+
+unsigned int inclinate(unsigned long pas, e_direction direction);
+
+unsigned int zoom(unsigned long pas, e_direction direction); // initialiser moteur et lance timer
+
+//**************************************************************************
+
+void TimerHandler_Rotation(unsigned long unused);	// controle du moteur de rotation
+
+void TimerHandler_Tilt(unsigned long unused); // controle des moteurs d'inclinaison
+
+void TimerHandler_Zoom(unsigned long unused); // controle des moteurs d'inclinaison
+
+//**************************************************************************
+
+int __init my_init (void);
 
 //**************************************************************************
 
@@ -77,7 +88,6 @@ void stopall(void);	// Cette fonction permet d'arreter immediatement tous les mo
 
 //**************************************************************************
 
-void __exit my_exit(void);
+void __exit my_exit (void);
 
 #endif /* ENGINECONTROL_H_ */
-
