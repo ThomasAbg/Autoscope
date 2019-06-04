bba1a9a6ec2d69851317fc8a71481789dcadadc7
ThomasAbg <01thomas.abgrall@gmail.com>
2019-06-04 22:24:52 +0200

    lenght Data ioctl

--- a4988.c
+++ a4988.c
@@ -516,7 +516,7 @@ static long etx_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 
 		case ROTATION:
 				printk(KERN_INFO "DRIVERMOTOR: receve order Rotation\n");						
-				copy_from_user(&Data, arg, sizeof(*Data);
+				copy_from_user(&Data, arg, 12;
 				printk(KERN_INFO "DRIVERMOTOR: order to do %d step in sens of rotation ", Data.nbPas);
 				// appel de la fonction pour faire tourner le moteur de rotation
 				if(Data.Sens == 0){
