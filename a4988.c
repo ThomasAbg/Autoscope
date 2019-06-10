e5ec662a437f6e7f242078e74bd3468494ed98a5
ThomasAbg <01thomas.abgrall@gmail.com>
2019-06-10 15:25:42 +0200

    modif ioctl

--- a4988.c
+++ a4988.c
@@ -128,7 +128,7 @@ static int etx_open(struct inode *inode, struct file *file);
 static int etx_release(struct inode *inode, struct file *file);
 static ssize_t etx_read(struct file *filp, char __user *buf, size_t len,loff_t * off);
 static ssize_t etx_write(struct file *filp, const char *buf, size_t len, loff_t * off);
-static long etx_ioctl(struct file *file, unsigned int cmd, struct Datat arg);
+static long etx_ioctl(struct file *file, unsigned int cmd, struct Data arg);
 //***********************************************************************************************//
 //***********************************************************************************************//
 
